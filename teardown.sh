b8f8af4f1cab093ca3847511df69e3215d3efe01
Sung Won Chung <sung@Sungs-MacBook-Pro.local>
2020-07-01 13:51:12 -0500

    delete secrets

--- teardown.sh
+++ teardown.sh
@@ -3,10 +3,14 @@
 # Script to teardown Kubernetes cluster on Docker Desktop for Mac Context
 
 echo "***********************"
-echo "Delete Kuberenetes Cluster Helm Deployment"
+echo "Delete Kuberenetes Cluster Helm Deployment and Secrets"
 echo "***********************"
 # delete helm deployment
 helm delete airflow
 
 # remove anything running on port 8080 for lingering airflow webserver deployment
 lsof -i:8080 -Fp | sed 's/^p//' | xargs kill -9
+
+# delete secrets for smoother setup if someone needs to change the service account
+kubectl delete secret dbt-secret
+kubectl delete secret gcr-key
