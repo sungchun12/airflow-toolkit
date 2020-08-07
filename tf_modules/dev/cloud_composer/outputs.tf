9bdb7d4858737df27e30b79047978821e16fcbd5
sungchun12 <sungwonchung3@gmail.com>
2020-08-07 15:20:45 -0500

    add output vars

--- tf_modules/dev/cloud_composer/outputs.tf
+++ tf_modules/dev/cloud_composer/outputs.tf
@@ -1,13 +1,19 @@
-# output "cluster_ca_certificate" {
-#   description = "The public certificate that is the root of trust for the cluster."
-#   sensitive   = true
-#   value       = module.gke_cluster.cluster_ca_certificate
-# }
+output "cloud_composer_id" {
+  description = "The exact id of the cloud composer environment"
+  value       = google_composer_environment.cloud-composer-env.id
+}
 
-# composer name
+output "cloud_composer_gke_cluster" {
+  description = "The exact id of the Kubernetes Engine cluster used to run this environment"
+  value       = google_composer_environment.cloud-composer-env.config[0].gke_cluster
+}
 
-# image version
+output "cloud_composer_dags_gcs_prefix" {
+  description = "The Cloud Storage prefix of the DAGs for this environment. Although Cloud Storage objects reside in a flat namespace, a hierarchical file tree can be simulated using '/'-delimited object name prefixes. DAG objects for this environment reside in a simulated directory with this prefix."
+  value       = google_composer_environment.cloud-composer-env.config[0].dag_gcs_prefix
+}
 
-# composer labels
-
-# composer web server url
+output "cloud_composer_airflow_uri" {
+  description = "The URI of the Apache Airflow Web UI hosted within this environment."
+  value       = google_composer_environment.cloud-composer-env.config[0].airflow_uri
+}
