c6a500fb2ae1817becb98f64ccc8dd9e41fa7adc
sungchun12 <sungwonchung3@gmail.com>
2020-08-07 16:33:21 -0500

    add parameters for networking module

--- tf_modules/dev/networking/main.tf
+++ tf_modules/dev/networking/main.tf
@@ -1,38 +1,34 @@
 #  https://cloud.google.com/vpc/docs/vpc#valid-ranges
 resource "google_compute_network" "network-composer" {
-  name                    = "composer-dev-network"
-  description             = "VPC network just for cloud composer"
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+  project                 = var.project
+  name                    = var.network_name
+  description             = var.network_description
+  auto_create_subnetworks = var.auto_create_subnetworks
+  routing_mode            = var.routing_mode
 }
 
 resource "google_compute_subnetwork" "subnetwork-composer" {
-  name          = "composer-dev-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1"
+  project       = var.project
+  name          = var.subnetwork_name
+  ip_cidr_range = var.ip_cidr_range
+  region        = var.subnetwork_region
   network       = google_compute_network.network-composer.id
 
-  secondary_ip_range = [
-    {
-      ip_cidr_range = "172.16.0.0/20" # cluster
-      range_name    = "gke-cluster"
-    },
-    {
-      ip_cidr_range = "172.16.16.0/24" # services
-      range_name    = "gke-services"
-    },
-  ]
+  secondary_ip_range = var.secondary_ip_range
 
-  private_ip_google_access = true
+  private_ip_google_access = var.private_ip_google_access
 }
 
 resource "google_compute_firewall" "iap-ssh-access" {
-  name        = "dev-bastion-ssh-firewall"
+  project     = var.project
+  name        = var.firewall_name
   network     = google_compute_network.network-composer.id
-  description = "Allow IAP to connect to VM instances"
-  direction   = "INGRESS"
-  priority    = 1000
+  description = var.firewall_description
+  direction   = var.firewall_direction
+  priority    = var.firewall_priority
 
+  # the below defaults are hard-coded as these are specific to identity aware proxy ssh enablement
+  # for more custom configs, you will have to make the below dynamic
   allow {
     protocol = "tcp"
     ports    = ["22"]
--- tf_modules/dev/networking/outputs.tf
+++ tf_modules/dev/networking/outputs.tf
@@ -1,24 +1,19 @@
-output "secondary_range_names" {
-  value       = [for i in google_compute_subnetwork.subnetwork-composer.secondary_ip_range : i["range_name"]]
-  description = "The names of the secondary ranges being created in one subnet"
-}
-
 output "network" {
-    value = google_compute_network.network-composer.id
-    description = "The exact id for the vpc network"
+  description = "The exact id for the vpc network"
+  value       = google_compute_network.network-composer.id
 }
 
 output "subnetwork" {
-    value = google_compute_subnetwork.subnetwork-composer.id
-    description = "The exact id for the vpc subnetwork"
+  description = "The exact id for the vpc subnetwork"
+  value       = google_compute_subnetwork.subnetwork-composer.id
 }
 
 output "cluster_secondary_range_name" {
-    value = google_compute_subnetwork.subnetwork-composer.secondary_ip_range[0]["range_name"]
-    description = "The exact id for the vpc subnetwork"
+  description = "The exact id for the cluster/pods secondary range name"
+  value       = google_compute_subnetwork.subnetwork-composer.secondary_ip_range[0]["range_name"]
 }
 
 output "services_secondary_range_name" {
-    value = google_compute_subnetwork.subnetwork-composer.secondary_ip_range[1]["range_name"]
-    description = "The exact id for the vpc subnetwork"
+  description = "The exact id for the services secondary range name"
+  value       = google_compute_subnetwork.subnetwork-composer.secondary_ip_range[1]["range_name"]
 }
