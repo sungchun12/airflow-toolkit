03362e2018feb753befad1a047b9c41c010149d4
sungchun12 <sungwonchung3@gmail.com>
2020-08-07 16:44:44 -0500

    add project var to all resources

--- tf_modules/dev/service_accounts/main.tf
+++ tf_modules/dev/service_accounts/main.tf
@@ -8,6 +8,7 @@
 
 ##### setup bastion host service account to be attached to compute engine VM #####
 resource "google_service_account" "service-account-bastion-host" {
+  project      = var.project
   account_id   = "bastion-host-dev-account"
   display_name = "Dev Service Account for accessing Composer Environment through bastion host"
   description  = "Service account for dev purposes to access a private Composer Environment and run airflow/kubectl cli commands"
@@ -21,6 +22,7 @@ locals {
 }
 
 resource "google_project_iam_binding" "bastion-host-entry" {
+  project  = var.project
   for_each = toset(local.bastion_service_account_roles)
   role     = each.value
 
@@ -33,6 +35,7 @@ resource "google_project_iam_binding" "bastion-host-entry" {
 #TODO: build conditions for all the iam policy bindings to be specific to this custom setup
 # download private key after terraform creates
 resource "google_service_account" "service-account-iap-ssh" {
+  project      = var.project
   account_id   = "service-account-iap-ssh"
   display_name = "A service account that can only ssh tunnel into the VM that then can access private IP composer"
   description  = "look at display name"
@@ -46,6 +49,7 @@ locals {
 }
 
 resource "google_project_iam_binding" "ssh-iap-compute-policy" {
+  project  = var.project
   for_each = toset(local.ssh_service_account_roles)
   role     = each.value
 
@@ -56,12 +60,14 @@ resource "google_project_iam_binding" "ssh-iap-compute-policy" {
 
 ##### setup service account to ensure composer environment is setup correctly #####
 resource "google_service_account" "service-account-composer" {
+  project      = var.project
   account_id   = "composer-dev-account"
   display_name = "Dev Service Account for Composer Environment"
   description  = "Service account for dev purposes to get a private Composer Environment running successfully"
 }
 
 resource "google_project_iam_member" "composer-worker" {
-  role   = "roles/composer.worker"
-  member = "serviceAccount:${google_service_account.service-account-composer.email}"
+  project = var.project
+  role    = "roles/composer.worker"
+  member  = "serviceAccount:${google_service_account.service-account-composer.email}"
 }
