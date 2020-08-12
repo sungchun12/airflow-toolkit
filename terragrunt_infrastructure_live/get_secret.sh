8bd51f2996acfe6b01c5abdd6f60721b11965201
sungchun12 <sungwonchung3@gmail.com>
2020-08-12 16:08:58 -0500

    setup project ID env var

--- terragrunt_infrastructure_live/get_secret.sh
+++ terragrunt_infrastructure_live/get_secret.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+gcloud config set project $PROJECT_ID
 export TERRAFORM_SECRET=$(gcloud secrets versions access latest --secret='terraform-secret')
 echo $TERRAFORM_SECRET > service_account.json
