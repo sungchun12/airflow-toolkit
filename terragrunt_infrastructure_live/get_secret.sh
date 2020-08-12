d888df620455563ba7c883e029305cc6639899ad
sungchun12 <sungwonchung3@gmail.com>
2020-08-12 11:29:10 -0500

    remove extra comment

--- terragrunt_infrastructure_live/get_secret.sh
+++ terragrunt_infrastructure_live/get_secret.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# gcloud secrets versions access latest --secret='terraform-secret'
+
 export TERRAFORM_SECRET=$(gcloud secrets versions access latest --secret='terraform-secret')
 echo $TERRAFORM_SECRET > service_account.json
