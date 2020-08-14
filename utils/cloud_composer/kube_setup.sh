55452a010bf2cdcd17a876a9745657a14c71fe92
sungchun12 <sungwonchung3@gmail.com>
2020-08-14 12:24:36 -0500

    update for extra secrets and installs

--- utils/cloud_composer/kube_setup.sh
+++ utils/cloud_composer/kube_setup.sh
@@ -2,7 +2,7 @@
 
 
 ### Access Specific Kubernetes Container from Cloud Shell
-sudo apt-get install kubectl
+sudo apt-get install kubectl git
 
 gcloud composer environments run dev-composer \
     --project wam-bam-258119 \
@@ -13,15 +13,30 @@ gcloud composer environments describe dev-composer \
     --location us-central1 \
     --format="value(config.gkeCluster)"
 
-gcloud container clusters get-credentials projects/wam-bam-258119/zones/us-central1-b/clusters/us-central1-dev-composer-22507e9d-gke \
+gcloud container clusters get-credentials projects/wam-bam-258119/zones/us-central1-b/clusters/us-central1-dev-composer-de094856-gke \
     --zone us-central1-b \
     --project wam-bam-258119
 
-kubectl create secret generic service-account --from-file=service_account.json
+# copy and paste contents of service account json file from local machine into the bastion host
+cat <<EOF > service_account.json
+<service account file contents>
+EOF
+
+kubectl create secret generic dbt-secret --from-file=service_account.json
+
+# Create SSH key pair for secure git clones
+ssh-keygen
+
+# copy and paste contents to your git repo SSH keys section
+# https://github.com/settings/keys
+cat ~/.ssh/id_rsa.pub
+
+# create the ssh key secret
+kubectl create secret generic ssh-key-secret --from-file=id_rsa=$HOME/.ssh/id_rsa --from-file=id_rsa.pub=$HOME/.ssh/id_rsa.pub
 
 kubectl get secrets
 
 kubectl get pods --all-namespaces
 
-kubectl -n composer-1-11-0-airflow-1-10-9-22507e9d \
-exec -it airflow-worker-67fcdf4d7c-28nnl -c airflow-worker -- /bin/bash
+kubectl -n composer-1-11-2-airflow-1-10-9-de094856 \
+exec -it airflow-worker-6f595f8779-8lbmv -c airflow-worker -- /bin/bash
