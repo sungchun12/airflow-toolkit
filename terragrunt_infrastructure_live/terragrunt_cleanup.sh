c0a0e1c5393677bb618b3740eb16abb0856db70a
Sung Won Chung <sung@Sungs-MacBook-Pro.local>
2020-08-19 00:16:37 -0500

    clean up all permutations of service account fiels

--- terragrunt_infrastructure_live/terragrunt_cleanup.sh
+++ terragrunt_infrastructure_live/terragrunt_cleanup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# you can run this locally 
+# you can run this locally
 # this is intended to be run in a CICD pipeline as an extra defense mechanism to prevent access to sensitive files
 
-# implementing this in an after-hook will not function properly for a terragrunt deployment with module output dependencies 
+# implementing this in an after-hook will not function properly for a terragrunt deployment with module output dependencies
 # as it will leave residual sensitive files within the dependencies
 
 # delete the terragrunt temp dirs
 find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
 
 # remove all sensitive files in current and subdirectories
-find . -type f -name "service_account.json" -prune -exec rm -f {} \;
+find . -type f -name "service_account*.json" -prune -exec rm -f {} \;
