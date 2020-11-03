bc3bae9bd45d6b277eb6925c2e508deafe43f02b
sungchun12 <sungwonchung3@gmail.com>
2020-11-03 17:13:24 -0600

    move command to dbt setup

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -6,12 +6,12 @@ from google.cloud import secretmanager
 # TODO(developer): update for your specific settings
 # GIT_REPO = "git@github.com:sungchun12/airflow-toolkit.git" #placeholder ssh git repo
 GIT_REPO = "github_sungchun12_airflow-toolkit"
+GIT_BRANCH = "feature-update-tutorial"
 PROJECT_ID = "big-dreams-please"
 DBT_IMAGE = f"gcr.io/{PROJECT_ID}/dbt_docker:dev-latest"
 
 env = os.environ.copy()
 DEPLOYMENT_SETUP = env["DEPLOYMENT_SETUP"]
-GIT_BRANCH = "master"
 
 
 def get_secret(project_name, secret_name):
@@ -85,12 +85,12 @@ pod_env_vars = {"PROJECT_ID": PROJECT_ID}
 git_clone_cmds = f"""
     /entrypoint.sh &&
     gcloud auth activate-service-account --key-file=account.json &&
-    gcloud source repos clone {GIT_REPO} --project={PROJECT_ID} &&
-    git checkout feature-update-tutorial"""
+    gcloud source repos clone {GIT_REPO} --project={PROJECT_ID}"""
 
 dbt_setup_cmds = f"""
     {git_clone_cmds} &&
     cd {GIT_REPO}/dbt_bigquery_example &&
+    git checkout {GIT_BRANCH} &&
     export PROJECT_ID={PROJECT_ID} &&
     export DBT_PROFILES_DIR=$(pwd) &&
     export DBT_GOOGLE_BIGQUERY_KEYFILE=/dbt/account.json"""
