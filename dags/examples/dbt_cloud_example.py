086e8a0ed5f63c4e96b194733ac40e6c686f367f
Sung Won Chung <sungwonchung3@gmail.com>
2021-07-01 16:25:17 -0500

    starting draft dag

--- dags/examples/dbt_cloud_example.py
+++ dags/examples/dbt_cloud_example.py
@@ -1,8 +1,40 @@
-# TODO: use the kubernetes pod operator
 # TODO: I want to import 2 things from the dbt_cloud_utils(class utility, default dbt Cloud variables)
-
-
-
 # Use the kubernetes pod operator or python virtualenv operator?
-
 # I should have one task
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from airflow_utils import DEPLOYMENT_SETUP, get_secret, set_google_app_credentials
+from dbt_cloud_utils import dbt_cloud_job_vars, dbt_cloud_job_runner
+
+
+# example dbt Cloud job config
+dbt_cloud_job_runner_config = dbt_cloud_job_runner(
+    account_id=4238, project_id=12220, job_id=12389
+)
+
+
+default_args = {
+    "owner": "airflow",
+    "email": ["airflow@example.com"],
+    "depends_on_past": False,
+    "start_date": datetime(2001, 1, 1),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "priority_weight": 1000,
+}
+
+
+with DAG(
+    "dbt_cloud_example", default_args=default_args, schedule_interval="@once"
+) as dag:
+
+    # Single task to execute dbt Cloud job
+    t1 = PythonOperator(
+        task_id="run-dbt-cloud-job",
+        python_callable=dbt_cloud_job_runner_config.run_job,
+        provide_context=True,
+    )
+
+    t1
