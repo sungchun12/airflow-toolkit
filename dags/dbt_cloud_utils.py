4a6689e100bf5a9ac9bb77c74bfaf20daf4da698
Sung Won Chung <sungwonchung3@gmail.com>
2021-07-01 15:58:42 -0500

    get class format

--- dags/dbt_cloud_utils.py
+++ dags/dbt_cloud_utils.py
@@ -28,7 +28,7 @@ API_KEY = os.getenv(
 )  # TODO: airflow variable vs. airflow secret vs. kubernetes secret?
 
 
-class DbtJobRunStatus(enum.IntEnum):
+class dbt_job_run_status(enum.IntEnum):
     """define a class of different dbt Cloud API status responses in integer format"""
 
     QUEUED = 1
@@ -43,56 +43,58 @@ class DbtJobRunStatus(enum.IntEnum):
 # TODO: log all the parameters passed to the instantiated class and compare to listing the job parameters from the actual dbt Cloud job and assert they match
 # TODO: create an overall class for extensibility to data share my dbt Cloud vars?
 # TODO: add a way to do command step overrides for the dbt Cloud job? No, let's keep it simple so as to bias towards changes in dbt Cloud
-# trigger the dbt Cloud pull request test job
-def _trigger_job() -> int:
-    res = requests.post(
-        url=f"https://cloud.getdbt.com/api/v2/accounts/{ACCOUNT_ID}/jobs/{JOB_ID}/run/",
-        headers={"Authorization": f"Token {API_KEY}"},
-        data={
-            "cause": f"BitBucket Pull Request",  # TODO: name of airflow DAG
-            "schema_override": f"dbt_cloud_pr_{BITBUCKET_BRANCH_SAFE_NAME}",  # TODO: remove this
-            "git_branch": f"{BITBUCKET_BRANCH}",  # TODO: Keep this?
-        },
-    )
 
-    try:
+
+class dbt_cloud_job_runner:
+    # trigger the dbt Cloud pull request test job
+    def _trigger_job(self) -> int:
+        res = requests.post(
+            url=f"https://cloud.getdbt.com/api/v2/accounts/{ACCOUNT_ID}/jobs/{JOB_ID}/run/",
+            headers={"Authorization": f"Token {API_KEY}"},
+            data={
+                "cause": f"{__file__}",  # name of the python file invoking this
+            },
+        )
+
+        try:
+            res.raise_for_status()
+        except:
+            print(f"API token (last four): ...{API_KEY[-4:]}")
+            raise
+
+        response_payload = res.json()
+        return response_payload["data"]["id"]
+
+    # to be used in a while loop to check on job status
+    def _get_job_run_status(self, job_run_id):
+        res = requests.get(
+            url=f"https://cloud.getdbt.com/api/v2/accounts/{ACCOUNT_ID}/runs/{job_run_id}/",
+            headers={"Authorization": f"Token {API_KEY}"},
+        )
+
         res.raise_for_status()
-    except:
-        print(f"API token (last four): ...{API_KEY[-4:]}")
-        raise
+        response_payload = res.json()
+        return response_payload["data"]["status"]
 
-    response_payload = res.json()
-    return response_payload["data"]["id"]
+    # main function operator to trigger the job and a while loop to wait for success or error
+    def run(self):
+        job_run_id = self._trigger_job()
 
+        print(f"job_run_id = {job_run_id}")
+        visit_url = f"https://cloud.getdbt.com/#/accounts/{ACCOUNT_ID}/projects/{PROJECT_ID}/runs/{job_run_id}/"
 
-# to be used in a while loop to check on job status
-def _get_job_run_status(job_run_id):
-    res = requests.get(
-        url=f"https://cloud.getdbt.com/api/v2/accounts/{ACCOUNT_ID}/runs/{job_run_id}/",
-        headers={"Authorization": f"Token {API_KEY}"},
-    )
+        while True:
+            time.sleep(5)
 
-    res.raise_for_status()
-    response_payload = res.json()
-    return response_payload["data"]["status"]
+            status = self._get_job_run_status(job_run_id)
 
+            print(f"status = {status}")
 
-# main function operator to trigger the job and a while loop to wait for success or error
-def run():
-    job_run_id = _trigger_job()
-
-    print(f"job_run_id = {job_run_id}")
-    visit_url = f"https://cloud.getdbt.com/#/accounts/{ACCOUNT_ID}/projects/{PROJECT_ID}/runs/{job_run_id}/"
-
-    while True:
-        time.sleep(5)
-
-        status = _get_job_run_status(job_run_id)
-
-        print(f"status = {status}")
-
-        if status == DbtJobRunStatus.SUCCESS:
-            print(f"Success! Visit URL: {visit_url}")
-            break
-        elif status == DbtJobRunStatus.ERROR or status == DbtJobRunStatus.CANCELLED:
-            raise Exception(f"Failure! Visit URL: {visit_url}")
+            if status == dbt_job_run_status.SUCCESS:
+                print(f"Success! Visit URL: {visit_url}")
+                break
+            elif (
+                status == dbt_job_run_status.ERROR
+                or status == dbt_job_run_status.CANCELLED
+            ):
+                raise Exception(f"Failure! Visit URL: {visit_url}")
