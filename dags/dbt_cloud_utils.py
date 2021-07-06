5019576533c1df2f3bcbf699ccc34821ca0f752a
Sung Won Chung <sungwonchung3@gmail.com>
2021-07-06 10:03:25 -0500

    immutable dataclasses and remove old comments

--- dags/dbt_cloud_utils.py
+++ dags/dbt_cloud_utils.py
@@ -6,31 +6,23 @@ from airflow.models import Variable
 
 
 # https://cloud.getdbt.com/#/accounts/4238/projects/12220/jobs/12389/
-@dataclass
+@dataclass(frozen=True)  # make attributes immutable
 class dbt_cloud_job_vars:
-    # slots create faster access to class attributes and can't add new attributes
-    __slots__ = "account_id", "project_id", "job_id", "cause"
+    """Basic dbt Cloud job configurations. Set attributes based on the ids within dbt Cloud URL"""
+
     # add type hints
     account_id: int
     project_id: int
     job_id: int
     cause: str
-    dbt_cloud_api_key: str = Variable.get("dbt_cloud_api_key")
+    dbt_cloud_api_key: str = Variable.get(
+        "dbt_cloud_api_key"
+    )  # manually set this in the airflow UI
 
 
-# API_KEY = os.getenv(
-#     "DBT_CLOUD_API_TOKEN"
-# )  # TODO: airflow variable vs. airflow secret vs. kubernetes secret?
-# kubernetes secret will only work in the context of kubernetes pod operator
-# I want to use google cloud secrets manager but that forces the developer to be GCP specifically
-# I'll try out the airflow environment variable as it will encrypt and cover it up locally
-# I recommend using a secrets backend like the Google Cloud one mentioned above for extra security and logging
-# Set this manually
-
-
-@dataclass
+@dataclass(frozen=True)
 class dbt_job_run_status:
-    """define a class of different dbt Cloud API status responses in integer format"""
+    """Different dbt Cloud API status responses in integer format"""
 
     QUEUED: int = 1
     STARTING: int = 2
@@ -38,12 +30,6 @@ class dbt_job_run_status:
     SUCCESS: int = 10
     ERROR: int = 20
     CANCELLED: int = 30
-
-
-# TODO: pass in this class to the functions below OR create this directly in the class and cut out this extra layer
-# TODO: log all the parameters passed to the instantiated class and compare to listing the job parameters from the actual dbt Cloud job and assert they match
-# TODO: create an overall class for extensibility to data share my dbt Cloud vars?
-# TODO: add a way to do command step overrides for the dbt Cloud job? No, let's keep it simple so as to bias towards changes in dbt Cloud
 
 
 class dbt_cloud_job_runner(dbt_cloud_job_vars, dbt_job_run_status):
